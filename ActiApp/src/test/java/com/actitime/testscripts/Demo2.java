12c3e151a4c0854b1d04ee4a85644330a494f886
Satish Krishna Rao <Satish Krishna Rao@DESKTOP-R8DAEJM>
2020-07-03 13:14:05 +0530

    Changes made

--- ActiApp/src/test/java/com/actitime/testscripts/Demo2.java
+++ ActiApp/src/test/java/com/actitime/testscripts/Demo2.java
@@ -1,5 +1,14 @@
+package com.actitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2
+{
+
 @Test
 	public void testDemo3()
 	{
 		Reporter.log("New test case added", true);
 	}
+}
