246028f6f4c49228c2c697332017e2ffe56b0065
Satish Krishna Rao <Satish Krishna Rao@DESKTOP-R8DAEJM>
2020-07-04 11:44:52 +0530

    first commit

---
+++ SatishDemo/src/test/java/TestCases/TestCase1.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void testTestCase1()
+	{
+		Reporter.log("Testcase 1 passed", true);
+	}
+
+}
---
+++ SatishDemo/src/test/java/TestCases/TestCase2.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+
+	@Test
+	public void testTestCase2()
+	{
+		System.out.println("test case 2 passed");
+	}
+}
